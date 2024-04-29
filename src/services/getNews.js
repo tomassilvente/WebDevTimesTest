6ec26e363673e96f4763ae4e3e11029b266f84a2
Tomas <silventetomas@gmail.com>
2024-04-29 16:07:38 -0300

    Last Update

--- src/services/getNews.js
+++ src/services/getNews.js
@@ -3,33 +3,31 @@ export const getNews = async (query = 'Elon Musk') =>{
     let articles = []
 
     //New Api Fetching
-    const NewsApiRes = await fetch(`https://newsapi.org/v2/top-headlines?q=${query}&from=2024-04-26&sortBy=publishedAt&apiKey=0d68055f5a3847c08722c9a4794cb598`)
+    const NewsApiRes = await fetch(`https://newsapi.org/v2/top-headlines?q=${query}&from=2024-04-26&sortBy=publishedAt&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`)
     const NewApiResponse = await NewsApiRes.json()
     const NewApiArticles = NewApiResponse.articles
-    console.log(NewApiArticles)
+   
     NewApiArticles?.slice(0,6).map(news =>(
         articles.push({by:'New Api Articles', key:news.title, author:news.author, description:news.description, publishedAt:news.publishedAt, url:news.url, title:news.title, category:news.category, image:news.urlToImage})
     ))
 
     //New York Times Fetching
-    const NYTRes = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=afLGeNpjFu8N8hOnWySqGtUjIxYECZ5J`)
+    const NYTRes = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${import.meta.env.VITE_NYT_API_KEY}`)
     const NYTResponse = await NYTRes.json()
     const NYTArticles = NYTResponse.response.docs
-    console.log(NYTArticles)
+    
     NYTArticles?.slice(0,6).map(news =>(
         articles.push({by:'New York Times',key:news.headline.main, author:news.byline.original, description:news.lead_paragraph, publishedAt:news.pub_date, url:news.web_url, title:news.headline.main, category:news.section_name, image:news.multimedia>0 ? news.multimedia[17].url : 'undefined'})
     ))
 
     //New York Times Fetching
-    const GuardianaApisRes = await fetch(`https://content.guardianapis.com/search?q=${query}&from-date=2014-04-26&api-key=1e2ff3ec-7371-4073-b1e5-82b53f97b383`)
+    const GuardianaApisRes = await fetch(`https://content.guardianapis.com/search?q=${query}&from-date=2014-04-26&api-key=${import.meta.env.VITE_GUARDIANA_API_KEY}`)
     const GuardianaApisResponse = await GuardianaApisRes.json()
     const GuardianaArticles = GuardianaApisResponse.response.results
-    console.log(GuardianaArticles)
+    
     GuardianaArticles?.slice(0,6).map(news =>(
         articles.push({by:'Guardiana', key:news.webTitle, author:'Guardiana', description:'Go to site!', publishedAt:news.webPublicationDate, url:news.webUrl, title:news.webTitle, category:news.sectionName})
     ))
-
-    console.log(articles)
 
     return articles
 }
